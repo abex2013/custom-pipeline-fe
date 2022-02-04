eb19e4367731f048da75cc6223dc28d19e68655f
abex2013 <81878810+abex2013@users.noreply.github.com>
2022-02-04 17:15:14 +0300

    signup new user is working correctly

--- src/app/core/models/post/signup-data.ts
+++ src/app/core/models/post/signup-data.ts
@@ -1,17 +1,24 @@
+// export interface SignupData {
+//   status: string
+//   token: string
+//   data: Data
+// }
+
+// export interface Data {
+//   user: User
+// }
+
+// export interface User {
+//   role: string
+//   _id: string
+//   name: string
+//   email: string
+//   __v: number
+// }
 export interface SignupData {
-  status: string
-  token: string
-  data: Data
-}
 
-export interface Data {
-  user: User
-}
-
-export interface User {
-  role: string
-  _id: string
   name: string
-  email: string
-  __v: number
+   email: string
+    password: string
+     confirmPassword: string
 }
--- src/app/core/services/signup.service.ts
+++ src/app/core/services/signup.service.ts
@@ -1,8 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { SignupData } from '../models/post/signup-data';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -11,31 +16,28 @@ export class SignupService {
   userSignupUrl = 'https://kafka-producer.herokuapp.com/api/v1/users/signup';
   userSignupInfo: BehaviorSubject<any> = new BehaviorSubject(null);
   jwtHelper = new JwtHelperService();
+  // httpOptions = {
+  //   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  // };
   constructor(protected _httpClient: HttpClient) { }
 
-  userLogin(userPayload: any):Observable<boolean> {
+
+  register(userPayload: any):Observable<any> {
+
     console.log(userPayload);
-    return this._httpClient.post(this.userSignupUrl, userPayload).pipe(
-      map((value:any) => {
-        if (value) {
-
-          localStorage.setItem('access_token', value.token);
-          localStorage.setItem('user_name', value.data.user.name);
-          const decryptedUser = this.jwtHelper.decodeToken(value.token);
-          console.log(decryptedUser);
-
-          const data = {
-            access_token: value.token,
-            userName: value.data.user.name,
-            userId: decryptedUser.id,
-            userIat: decryptedUser.iat,
-            tokenExpiration: decryptedUser.exp,
-          };
-          this.userSignupInfo.next(data);
-          return true;
-        }
-        return false;
-      })
-    );
+    //console.log( this._httpClient.post(this.userSignupUrl,signupData).pipe(catchError(this.handleError)))//, httpOptions);
+    return this._httpClient.post(this.userSignupUrl,userPayload,httpOptions).pipe(catchError(this.handleError));
+    return this._httpClient.post(this.userSignupUrl,userPayload);
+  }
+  handleError(error: HttpErrorResponse) {
+    let msg = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      msg = error.error.message;
+    } else {
+      // server-side error
+      msg = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    return throwError(msg);
   }
 }
